dc38b591dfef7f3e4c219f0fc4d5b711fdd779fa
SudhamshBachu <97125757+SudhamshBachu@users.noreply.github.com>
2022-02-03 09:00:06 -0800

    Update FraudDetectionService.java

--- src/main/java/com/arkose/ci/services/FraudDetectionService.java
+++ src/main/java/com/arkose/ci/services/FraudDetectionService.java
@@ -4,7 +4,7 @@ public class FraudDetectionService {
 	
 	//Hard coded IP test
 	public boolean isIpValid(String ip) {
-		if(ip.equals("127.0.0.1  ")) {
+		if(ip.equals("127.0.0.1")) {
 			return false;
 		}
 		
